18f54530aa48b9d21e5493c68b694466c778e6fd
Marvey1203 <102023269+Marvey1203@users.noreply.github.com>
2023-09-30 20:41:05 +0200

    Still not confident that this worked

--- app/shop/[slug]/page.tsx
+++ app/shop/[slug]/page.tsx
@@ -41,7 +41,7 @@ export async function generateStaticParams(): Promise<any[]> {
     const res = await getAllProducts();
     const data: ProductData = res.body.data.products.edges;
     console.log(data); // Log the data to check its structure
-    const arr: Promise<any[]> = data.map((product: Product) => {
+    const arr = data.map((product: Product) => {
       return { slug: product.node.handle };
     });
     return arr
